9d0b41fa6def19fda93f96fb923c5cc3a1e0bbfe
Eike Caldeweyher <eike@safe.global>
2025-05-02 14:38:20 +0200

    chore: add dynamic set builder and chain mapping

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
+import os
+
+from typing import Set
+
 # define dune blockchains:
 ABSTRACT = 2741
 APECHAIN = 33139
@@ -42,47 +47,144 @@ ZKSYNC = 324
 ZORA = 7777777
 
 dune_supported_blockchains = set(
-    ABSTRACT,
-    APECHAIN,
-    ARBITRUM_ONE,
-    ARBITRUM_NOVA,
-    AVALANCHE,
-    B3,
-    BERACHAIN,
-    BLAST,
-    BNB_MAINNET,
-    BOB,
-    BOBA,
-    CELO,
-    CORN,
-    DEGEN,
-    ETHEREUM_MAINNET,
-    FANTOM_OPERA,
-    FLARE,
-    GNOSIS,
-    INK,
-    KAIA,
-    LENS,
-    LINEA,
-    MANTLE,
-    NEAR,
-    OP_BNB,
-    OP_MAINNET,
-    PLUME_MAINNET,
-    POLYGON_MAINNET,
-    POLYGON_ZKEVM,
-    RONIN,
-    SCROLL,
-    SEI,
-    SHAPE,
-    SONIC,
-    SOPHON,
-    TRON,
-    UNICHAIN,
-    VICTION,
-    WORLDCHAIN,
-    ZKSYNC,
-    ZORA
+    [
+        ABSTRACT,
+        APECHAIN,
+        ARBITRUM_ONE,
+        ARBITRUM_NOVA,
+        AVALANCHE,
+        B3,
+        BERACHAIN,
+        BLAST,
+        BNB_MAINNET,
+        BOB,
+        BOBA,
+        CELO,
+        CORN,
+        DEGEN,
+        ETHEREUM_MAINNET,
+        FANTOM_OPERA,
+        FLARE,
+        GNOSIS,
+        INK,
+        KAIA,
+        LENS,
+        LINEA,
+        MANTLE,
+        NEAR,
+        OP_BNB,
+        OP_MAINNET,
+        PLUME_MAINNET,
+        POLYGON_MAINNET,
+        POLYGON_ZKEVM,
+        RONIN,
+        SCROLL,
+        SEI,
+        SHAPE,
+        SONIC,
+        SOPHON,
+        TRON,
+        UNICHAIN,
+        VICTION,
+        WORLDCHAIN,
+        ZKSYNC,
+        ZORA,
+    ]
 )
 
+# sanity check that ethereum mainnet is in the set
+assert 1 in dune_supported_blockchains
 
+# define the mapping
+bc_mapping = {
+    ABSTRACT: "ABSTRACT",
+    APECHAIN: "APECHAIN",
+    ARBITRUM_ONE: "ARBITRUM_ONE",
+    ARBITRUM_NOVA: "ARBITRUM_NOVA",
+    AVALANCHE: "AVALANCHE",
+    B3: "B3",
+    BERACHAIN: "BERACHAIN",
+    BLAST: "BLAST",
+    BNB_MAINNET: "BNB_MAINNET",
+    BOB: "BOB",
+    BOBA: "BOBA",
+    CELO: "CELO",
+    CORN: "CORN",
+    DEGEN: "DEGEN",
+    ETHEREUM_MAINNET: "ETHEREUM_MAINNET",
+    FANTOM_OPERA: "FANTOM_OPERA",
+    FLARE: "FLARE",
+    GNOSIS: "GNOSIS",
+    INK: "INK",
+    KAIA: "KAIA",
+    LENS: "LENS",
+    LINEA: "LINEA",
+    MANTLE: "MANTLE",
+    NEAR: "NEAR",
+    OP_BNB: "OP_BNB",
+    OP_MAINNET: "OP_MAINNET",
+    PLUME_MAINNET: "PLUME_MAINNET",
+    POLYGON_MAINNET: "POLYGON_MAINNET",
+    POLYGON_ZKEVM: "POLYGON_ZKEVM",
+    RONIN: "RONIN",
+    SCROLL: "SCROLL",
+    SEI: "SEI",
+    SHAPE: "SHAPE",
+    SONIC: "SONIC",
+    SOPHON: "SOPHON",
+    TRON: "TRON",
+    UNICHAIN: "UNICHAIN",
+    VICTION: "VICTION",
+    WORLDCHAIN: "WORLDCHAIN",
+    ZKSYNC: "ZKSYNC",
+    ZORA: "ZORA",
+}
+
+
+def load_deployed_chains(fname: str, deployment: str) -> Set[int]:
+    # check if deoloyment is valid
+    if deployment not in ["canonical", "eip155", "zksync"]:
+        return set()
+
+    # set path of the file
+    path = os.path.join(os.path.dirname(__file__), f"deployed_chains/{fname}")
+
+    # check if the file exists
+    if not os.path.exists(path):
+        return set()
+
+    # load the file
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    # iterate over networks
+    networks = data["networkAddresses"]
+    valid_networks = set()
+
+    for k, v in networks.items():
+        # try to convert the key to an int
+        k = int(k)
+
+        if k not in dune_supported_blockchains:
+            continue
+
+        if type(v) is str:
+            if v == deployment:
+                valid_networks.add(k)
+        elif type(v) is list:
+            for n in v:
+                if n == deployment:
+                    valid_networks.add(k)
+    return valid_networks
+
+
+def main():
+    # canonical
+    safe_proxy_factory_v1_3_0 = load_deployed_chains(
+        "proxy_factory_v1_3_0.json", "canonical"
+    )
+    print(safe_proxy_factory_v1_3_0)
+
+
+if __name__ == "__main__":
+    main()
